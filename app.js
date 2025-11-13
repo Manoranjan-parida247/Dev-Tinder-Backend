d0e78273ae3801e6771774dad430a15760b66ac6
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2025-11-13 11:33:33 +0530

    playing with routes and ep-4

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res)=> {
-    res.send("Hello from the server");
+app.use("/", (req, res)=> {
+    res.send("Hello World");
 })
 
+app.use("/hello", (req, res)=> {
+    res.send("Hello Route");
+})
+
+app.use("/test", (req, res)=> {
+    res.send("Test Route");
+})
 app.listen(3000, ()=>{
     console.log("Server is Running on PORT 3000");
 })

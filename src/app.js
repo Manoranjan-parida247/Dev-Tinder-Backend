741d1d0dc07ae156f0d861bc356a08ec4e60b93a
Manoranjan-parida247 <manoranjanparida247@gmail.com>
2025-11-20 18:57:55 +0530

    episode 7

--- src/app.js
+++ src/app.js
@@ -3,31 +3,159 @@ const connectDB = require("./config/database")
 const app = express();
 const User = require("./models/user")
 
-app.post("/signup", async(req, res) => {
+app.use(express.json()); // the logic of processing json data
 
-    console.log(req)
-    const userObj = {
-        firstName: "Manoranjan",
-        lastName: "Parida",
-        emailId: "mano@gmail.com",
-        password:"Muna@123"
-    }
 
-    //creating a new instance of User model
-    const user = new User(userObj);
-
+// get user by email
+app.get("/user", async (req, res) => {
     try {
-        res.status(201).json({
-        statusCode: 201,
-        message: "User added successfully!!"
-    })
-    } catch (error) {
-        
-    }
-    await user.save();
+        const emailId = req.body.emailId;
+        if (!emailId) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Email id is required!"
+            })
+        }
+        const user = await User.findOne({ emailId: emailId });
+        console.log(user)
+        if (!user) {
+            res.status(404).json({
+                statusCode: 404,
+                message: "User not found"
+            })
+        }
 
-    
+        res.status(200).json({
+            statusCode: 200,
+            message: "user found",
+            user
+        })
+    } catch (err) {
+        res.status(500).json({
+            statusCode: 500,
+            message: "Internal Server Error",
+            error: err.message
+        })
+    }
 })
+
+// Feed api : GET / feed -> Fet all the users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+
+        if (users.length === 0) {
+            return res.status(200).json({
+                statusCode: 200,
+                message: "No user available , please add some user",
+                data: []
+            })
+        }
+
+        res.status(200).json({
+            statusCode: 200,
+            message: "Users fetched successfull",
+            data: users
+        })
+    } catch (error) {
+        res.status(500).json({
+            statusCode: 500,
+            message: "Something went wrong",
+            error: error.message
+        });
+    }
+});
+
+//update api || patch
+app.patch("user/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        if (!id) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Id is required"
+            })
+        }
+
+        const updateData = req.body;
+
+        const ALLOWED_UPDATES = ["fullName", "age", "photoUrl", "skills", "about", "gender"];
+        const isUpdateAllowed = Object.keys(updateData).every((k) => ALLOWED_UPDATES.includes(k));
+
+        if (!isUpdateAllowed) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Update is not allowed"
+            })
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(id, updateData, { returnDocument: "after", runValidators: true });
+        console.log("updated user:", updatedUser);
+
+        if (!updatedUser) {
+            return res.status(400).json({
+                statusCode: 404,
+                message: "User not found"
+            })
+        }
+
+        res.status(200).json({
+            statusCode: 200,
+            message: "User updated successfully",
+            data: updatedUser
+        })
+    } catch (err) {
+        res.status(500).json({
+            statusCode: 500,
+            message: "Internal server error",
+            error: err.message,
+        })
+    }
+})
+
+// Delete API
+app.delete("/user/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        if (!id) {
+            return res.status(400).json({
+                statusCode: 400,
+                message: "Id is required"
+            })
+        }
+
+        const deletedUser = await User.findByIdAndDelete(id);
+
+        if (!deletedUser) {
+            return res.status(404).json({
+                statusCode: 404,
+                message: "User not found",
+            })
+        }
+
+        res.status(200).json({
+            statusCode: 200,
+            message: "User deleted successsfully",
+            deletedUser
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            statusCode: 500,
+            message: "Something went wrong",
+            error: error.message
+        })
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        statusCode: 500,
+        message: "Something went wrong on the server!"
+    });
+});
+
 
 // first connected to database then start server and 
 connectDB().then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,57 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String
+    fullName: {
+        type: String,
+        required: true,
+        minLength:3,
+        maxLength: 30,
+        trim: true,
     },
-    firstName:{
-        type: String
-    },
-    lastName:{
-        type: String
+    emailId:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim: true,
+
+
     },
     password:{
-        type: String
+        type: String,
+        required: true,
     },
     age:{
-        type: Number
+        type: Number,
+        min: 18,
+        max: 80
     },
     gender:{
-        type: String
+        type: String,
+        lowercase: true,
+        //custom validation
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error ("Invalid gender data");
+            }
+        }
     },
+    photoUrl:{
+        type: String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOeZjZWEr4oFmJhILQQgTy7-WUX9BmRrAAFw&s"
+
+    },
+    about:{
+        type: String,
+        default: "This is default about of the user!"
+    },
+    skills:{
+        type: [String]
+    }
+
+}, 
+{
+    timestamps: true
 })
 
 const User = mongoose.model("User", userSchema);
